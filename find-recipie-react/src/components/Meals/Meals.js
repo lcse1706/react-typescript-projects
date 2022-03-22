cddabbde6c876bbd02e542733a8f18bea98b5362
Lukasz Czarniecki <lcse.2001@gmail.com>
2022-03-22 11:29:31 +0100

    Search result styled

--- find-recipie-react/src/components/Meals/Meals.js
+++ find-recipie-react/src/components/Meals/Meals.js
@@ -1,15 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  list-style: none;
+
+  & li {
+    width: 22%;
+    margin-top: 1rem;
+    position: relative;
+  }
+
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+
+  & .meal-info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-weight: 500;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    opacity: 0;
+    transition: opacity 0.4s ease;
+  }
+
+  & .meal-info:hover {
+    opacity: 1;
+  }
+`;
 
 const Meals = props => {
   return (
-    <ul>
+    <List>
       {props.meals.map(meal => (
         <li>
-          <p>{meal.name}</p>
+          <p className='meal-info'>{meal.name}</p>
           <img src={meal.img}></img>
         </li>
       ))}
-    </ul>
+    </List>
   );
 };
 
