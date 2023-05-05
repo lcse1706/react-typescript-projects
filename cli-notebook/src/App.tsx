8a8332b4ccab205d1c0134b513f36ec1c82acea6
Lukas Czarniecki <lcse.2001@gmail.com>
2023-05-05 19:36:35 +0200

    MarkDown editor added and styled

--- cli-notebook/src/App.tsx
+++ cli-notebook/src/App.tsx
@@ -1,11 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-
-import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
+// import CodeCell from './components/code-cell';
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
